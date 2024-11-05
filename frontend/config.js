b2eb41f2f5467ae0dc3b8f29faa733fa5885c54d
Patrik <pbozuric@outlook.com>
2024-11-05 08:42:17 +0100

    Update API URL to point to EC2 backend

--- frontend/config.js
+++ frontend/config.js
@@ -1,1 +1,2 @@
-window.API_URL = 'http://localhost:5000';  // for local development
+//window.API_URL = 'http://localhost:5000';  // for local development
+window.API_URL = 'http://52.28.158.243:5000';  // Your EC2 public IP
